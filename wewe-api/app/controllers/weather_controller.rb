224d64bc315d25b26fc5300b411a5cac6f1d9780
luis freitas <freitasgluis@gmail.com>
2022-05-24 02:32:37 +0200

    extracting geolocation from openweather and creating api enpoint

--- wewe-api/app/controllers/weather_controller.rb
+++ wewe-api/app/controllers/weather_controller.rb
@@ -1,2 +1,30 @@
 class WeatherController < ApplicationController
+  def location
+    @location_query = params[:city]
+
+    # Extracting Lat and Lon from API
+    require 'net/http'
+    require 'json'
+
+    @url = "https://api.openweathermap.org/geo/1.0/direct?q=#{@location_query}&limit=5&appid=ecd1d928cecc524245b088ed57bc6b99"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+    # Passing Lat and Lon to Weather
+    @weather = []
+
+    @output.each do |loc|
+      @url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{loc['lat']}&lon=#{loc['lon']}&units=metric&exclude=minutely,hourly,alerts&appid=ecd1d928cecc524245b088ed57bc6b99"
+      @uri = URI(@url)
+      @response = Net::HTTP.get(@uri)
+      @output2 = JSON.parse(@response)
+      @weather.push(@output2)
+    end
+
+    render json: @weather.as_json(only: ["current", "temp", "weather", "main", "description", "daily", "min", "max", "humidity", "wind_speed", "pop"])
+
+    # respond_to do |format|
+    #   format.json { render json: @weather.as_json }
+    # end
+  end
 end
