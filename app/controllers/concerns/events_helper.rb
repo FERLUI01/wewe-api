b7819ada32b460a62d85158704d2c17b6ec977a9
luis freitas <freitasgluis@gmail.com>
2022-06-09 14:57:32 +0200

    namespace api and moved controller into api folder

--- app/controllers/concerns/events_helper.rb
+++ app/controllers/concerns/events_helper.rb
@@ -2,17 +2,17 @@ module EventsHelper
 
   def compute_clothes(min_temp, max_temp)
     if min_temp <= 5 && max_temp <= 10
-      'Wear warm clothes.'
+      'Wear warm clothes'
     elsif min_temp <= 10 && max_temp <= 15
-      'Wear a jacket and a thick sweater.'
+      'Wear a jacket and a thick sweater'
     elsif min_temp <= 10 && max_temp >= 15
-      'Wear a jacket and a light sweater.'
+      'Wear a jacket and a light sweater'
     elsif min_temp >= 10 && max_temp <= 20
-      'Take a sweater.'
+      'Take a sweater'
     elsif min_temp <= 20 && max_temp >= 20
-      'Wear light clothes and maybe a sweater.'
+      'Wear light clothes and maybe a sweater'
     elsif min_temp >= 20 && max_temp >= 20
-      'Wear light clothes.'
+      'Wear light clothes'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'weather', to: 'weather#index'
+  namespace :api, defaults: { format: :json } do
+    get 'weather', to: 'weather#index'
+  end
 end
